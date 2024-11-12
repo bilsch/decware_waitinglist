a33293729285c9af90ada61e2adfa4e6fe361fb1
Bill Schwanitz <bilsch@bilsch.org>
2024-11-12 13:56:01 -0500

    add order time tracker

---
+++ src/order_fullfulment_dumper.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from pymongo import MongoClient
+from Config import Config
+
+print("connect to mongo")
+mc = MongoClient(Config().mongo_uri())
+db = mc.get_database(Config().database_name())
+
+# collections we'll call
+entry_col = db.get_collection("entries")
+log_col = db.get_collection("log")
+
+complete_amps = entry_col.find({"status": "Complete"})
+
+for amp in complete_amps:
+    id = amp.get("_id")
+    order_date = amp.get("date")
+    log = log_col.find_one({"entry_owner": id, "status": "Complete"})
+    complete_date = log.get("date")
+    diff = complete_date - order_date
+    print(f"{id} order date: {order_date} complete date: {complete_date} - diff: {diff}")
